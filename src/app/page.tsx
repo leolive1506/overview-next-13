8e543c27c9b42d77550ee9bbf7ea161f3fff842b
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-12 11:28:31 -0300

    add loading component with Suspense api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Repo } from "@/components/Repo";
 import { User } from "@/components/User";
 import Link from "next/link";
+import { Suspense } from "react";
 export const revalidate = 30
 
 export default function Home() {
@@ -8,8 +9,12 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <User />
-      {/* <Repo /> */}
+      <Suspense fallback={<p>Carregando usuários...</p>}>
+        <User />
+      </Suspense>
+      <Suspense fallback={<p>Carregando repositórios...</p>}>
+        <Repo />
+      </Suspense>
     </div>
   )
 }
--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,4 +1,5 @@
 export async function Repo() {
+  await new Promise(resolve => setTimeout(resolve, 5000))
   const response = await fetch('http://api.github.com/users/leolive1506/repos', {
     next: {
       revalidate: 30
